6d8f85e9262781551ba4d9d5801a812855d4dee6
Andriy <andriymattt@gmail.com>
2019-12-09 13:29:55 +0200

    feature/droid: add class Droid that extends a class DroidParent

---
+++ src/main/java/com/epam/droid/Droid.java
@@ -0,0 +1,9 @@
+package com.epam.droid;
+
+public class Droid extends DroidParent {
+    public int power;
+    public Battery battery;
+    public transient int health;
+    public static int staticValue;
+
+}
