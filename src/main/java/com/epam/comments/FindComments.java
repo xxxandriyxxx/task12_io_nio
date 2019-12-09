2be030879599537b74d9c6dc28b2ef4fff300c80
Andriy <andriymattt@gmail.com>
2019-12-09 20:50:47 +0200

    feature/comments: add a method of finding and printing comments

--- src/main/java/com/epam/comments/FindComments.java
+++ src/main/java/com/epam/comments/FindComments.java
@@ -1,4 +1,25 @@
 package com.epam.comments;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class FindComments {
+
+    public static void printComments(String filePath) throws IOException {
+        Scanner sc = new Scanner(new File(filePath));
+        String input;
+        while (sc.hasNextLine()) {
+            input = sc.nextLine();
+            if (input.contains("//")) {
+                System.out.println(input.substring(input.indexOf("//")));
+            }
+            if (input.contains("/*")) {
+                System.out.println(input.substring(input.indexOf("/*")));
+            } else if (input.contains("*")) {
+                System.out.println(input.substring(input.indexOf("*")));
+            }
+        }
+    }
+
 }
